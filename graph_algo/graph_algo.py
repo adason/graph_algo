b13cc7f245db821df763224ebcaa089e2f22539b
adason <tdadason@gmail.com>
2014-08-03 16:40:50 -0700

    Add CLI

--- graph_algo/graph_algo.py
+++ graph_algo/graph_algo.py
@@ -2,20 +2,23 @@
 """Main program for Command Line interface
 
 Usage:
-    graph_algo [-hv] (-i <file>)
+    graph_algo -h
     graph_algo --version
+    graph_algo p2hw4 [--argo <argo>] [-v] -i <file>...
 
 Options:
-    -h --help             show this help message and exit
-    --version             show version and exit
-    -v --verbose          print status messages
-    -i, --input <file>    input file
+    -h --help                show this help message and exit
+    --version                show version and exit
+    -v --verbose             print status messages
+    -i, --input <file>...    input file(s)
+    --argo <algo>            choose one algorithm
 
 """
 
 
 from __future__ import unicode_literals, print_function
 from docopt import docopt
+from .graph import Graph
 
 
 __version__ = "0.1.0"
@@ -27,4 +30,16 @@ def main():
     """Main entry point for the graph_algo CLI.
     """
     args = docopt(__doc__, version=__version__)
-    print(args)
+    # print(args)
+
+    if args["p2hw4"]:
+        min_dists = []
+        for fn in args["--input"]:
+            g = Graph.read_input_part2_hw4(fn)
+            dist_pairs = g.floyd_warshall_apsp()
+            if dist_pairs:
+                min_dists.extend(dist_pairs.values())
+        if len(min_dists) > 0:
+            print(min(min_dists))
+        else:
+            print("NULL")
